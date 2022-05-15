66fe2aae3327dfae059089cc3145416933254f71
temchukwu <godblesschuks@yahoo.com>
2022-05-15 13:18:24 +0100

    struct

---
+++ temchuks.js
@@ -0,0 +1,177 @@
+// Append items with the push function
+var ourArray = ["Stimpson", "j", "cat"]
+ourArray.push(["happy", "Joy"]);
+console.log(ourArray);
+
+let myList = ["cereal", 3];
+myList.unshift();
+console.log(myList);
+myList.length
+
+function force()  {
+    console.log("mass*acceleration");
+}
+force();
+
+function momentum () {
+    console.log("mass*velocity");
+}
+momentum();
+function mySchool() {
+    console.log("staff + students");
+}
+mySchool()
+
+// Passing values with parameters and argument
+function myNumb(a, b)  {
+    console.log(a-b);
+}
+myNumb(9, 3);
+
+function Jesus(x, y)  {
+    console.log(x+y);
+}
+Jesus(8, 6);
+
+function subtraction(c, d){
+    console.log(d-c);
+}
+subtraction(15, 3);
+
+// Local Variable
+function Scope()  {
+    let Scope = 9;
+    console.log(Scope);
+}
+    Scope();
+
+
+    function greece()  {
+        let greece = "Canada";
+        console.log("greece");
+    }
+    greece();
+
+    var outerWear = "T-Shirt";
+    function myoutFit()  {
+        return outerWear;
+    }
+    console.log(myoutFit());
+
+    function shirt()  {
+        let myWear = "Sweater";
+        return myWear;
+    }
+    console.log(shirt());
+    // Function with Boolean
+    function trueOrFalse(wasThatTrue)  {
+        if (wasThatTrue) {
+            return "Yes, that was true";
+        }
+        return "No, that was false";
+    }
+    console.log(trueOrFalse(true));
+    console.log(trueOrFalse(false))
+
+    function testEqual(val) {
+        if (val == 12) {
+            return "Equal";
+        }
+        return "Not Equal";
+    }
+    console.log(testEqual(10));
+console.log(testEqual(15));
+
+function comparison(s, f)  {
+    if (s === f) {
+        return "True";
+    }
+    return "False";
+}
+console.log(comparison(13, 13));
+
+function Equality(n, m)  {
+    if (n !=m)  {
+        return "Yes";
+    }
+    return "No";
+}
+console.log(Equality(8, 3));
+console.log(Equality(15, 15));
+
+function Logical(p, q) {
+    if (p > q) {
+        return "Great";
+    }
+    return "Sall";
+}
+console.log(Logical(21, 18));
+console.log(Logical(16, 19));
+
+// Object variable
+let myPlant = {
+    type: "flower",
+    list:[
+        "rose",
+        "tulip",
+        "dandelion",
+    ]
+}
+    console.log(myPlant);
+    console.log(myPlant.length)
+
+    // Record Label
+    let myMusic = {
+        artist: "P_Square",
+        list: [
+            "Beautiful Onyinye",
+            "Do me I do you",
+            "Mama Africal",
+            "Paddey",
+        ]
+    }
+            console.log(myMusic);
+            
+
+               
+                
+                
+       
+
+            
+
+
+            
+
+                
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
